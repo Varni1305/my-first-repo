b66a7097a1f0caca4a30e116bcc4381a1686075d
Varni1305 <varnipaghadar13@gmail.com>
2026-01-16 08:56:57 +0530

    Add hello.c with a simple Hello World program

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    printf("This is my first GitHub repository.\n");
+    printf("Learning C, Linux and GitHub.\n");
+
+    return 0;
+}
